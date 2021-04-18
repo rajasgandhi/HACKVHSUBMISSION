321859a8efc92bdf28ea03b5b5838576b4a89684
Rajas Gandhi <rajasmgandhi@gmail.com>
2021-04-18 04:06:22 -0500

    init

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import _ from "lodash";
 
 export default function App() {
+  const [value, setValue] = useState("A doctor a day keeps an apple away");
+  const [author, setAuthor] = useState("Rajas Gandhi");
+
+  const newone = async () => {
+    const resp = await fetch("https://type.fit/api/quotes");
+    const respJSON = await resp.json();
+    var firstRandomElement = _.shuffle(respJSON)[0];
+    setValue(firstRandomElement.text);
+    setAuthor(firstRandomElement.author);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{value}</Text>
+      <Text>{author}</Text>
+      <TouchableOpacity
+        style={{
+          height: "8%",
+          width: "60%",
+          borderRadius: 20,
+          borderWidth: 1,
+          borderColor: "white",
+          backgroundColor: "#add8e6",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        onPress={newone}
+      >
+        <Text style={{ color: "white", fontSize: 30 }}>New Quote</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
